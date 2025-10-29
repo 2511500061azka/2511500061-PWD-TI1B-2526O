cff1626f43ad5ddf577357a28a2464ab7ebc8846
2511500091-aidil <2511500091@mahasiswa.atmaluhur.ac.id>
2025-10-29 15:18:23 +0700

    membuat script.js

--- pertemuan-05/script.js
+++ pertemuan-05/script.js
@@ -9,42 +9,46 @@ document.getElementById("menuToggle").addEventListener("click", function () {
     }
 });
 
-document.querySelector("form").addEventListener("submit", function (e){
+document.querySelector("form").addEventListener("submit", function (e) {
     const nama = document.getElementById("txtNama");
     const email = document.getElementById("txtEmail");
-    const pesan = document.getElementById("txtPesan")
-
-    document.querySelectorAll(".error-msg").forEach(eL => eL.remove());
+    const pesan = document.getElementById("txtPesan");
+    
+    document.querySelectorAll(".error-msg").forEach(el => el.remove());
     [nama, email, pesan].forEach(eL => eL.style.border = "");
 
     let isValid = true;
 
-    if (nama.value.trim(). lenght < 3) {
-        showError(nama, "Nama minimal 3 haruf dqan tidak boleh kosong.");
+    if (nama.value.trim().legth < 3) {
+        showError(nama, "Nama minimal 3 huruf dan tidak boleh kosong.");
         isValid = false;
-    } else if (!/^[A-Za-z\s]+$/.test(nama.value)) {
+    }   else if (!/^[A-Za-z\s]+$/.test(nama.velue)) {
         showError(nama, "Nama hanya boleh berisi huruf dan spasi.");
+        isValid = false;
+    }
     }
 
     if (email.value.trim() === "") {
         showError(email, "Email wajib diisi.");
         isValid = false;
-    } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email.value)) {
-        showError(email, "Format email tidak valid, Contoh: nama@mail.com");
+    } else if (!/^[^\s@+@]+@[^\s@]+\.[^\s@]+$/.test(email.value)) {
+        showError(email, "Format email tidak valid. Contoh: nama@mail.com");
         isValid = false;
     }
 
-    if (pesan.value.trim().lenght < 10) {
+    if (pesan.value.trim().length < 10) {
         showError(pesan, "Pesan minimal 10 karakter agar lebih jelas.");
-        isValid = false;
+
     }
 
     if (!isValid) {
         e.preventDefault();
     } else {
-        alert("terima kasih, " + nama.value + "!\nPesan Anda telah dikirim.");
+        alert("Terima kasih, " + nama.value + "!\nPesan anda telah dikirim.");
     }
 });
+
+
 
 function showError(inputElement, message) {
     const label = inputElement.closest("label");
